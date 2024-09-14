bac58cbabfd3e68ea201d4843800cf1f02d6e1d2
shadowfieng <shadowfieng@gmail.com>
2024-09-14 15:10:38 +0700

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { Layout } from 'antd'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 export const App = () => {
   return (
     <Layout>
       <Layout.Header style={{ backgroundColor: 'grey', color: 'white' }}>
-        Tristar
+        <Link to='/' style={{ color: 'black' }}>
+          Tristar
+        </Link>
       </Layout.Header>
-      <Layout.Content style={{ padding: '2rem' }}>
+      <Layout.Content
+        style={{
+          padding: '2rem',
+          height: 'calc(100dvh - 64px)',
+          overflowY: 'auto',
+        }}
+      >
         <Outlet />
       </Layout.Content>
     </Layout>
