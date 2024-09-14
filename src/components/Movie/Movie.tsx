72bbda3866f3d0369f40be4f59a4f5c4973d6272
shadowfieng <shadowfieng@gmail.com>
2024-09-14 18:16:58 +0700

    feat: add favorites ui logic

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,18 +1,65 @@
+import { HeartFilled, HeartOutlined } from '@ant-design/icons'
 import { Card } from 'antd'
 import Meta from 'antd/es/card/Meta'
+import { favoritesSlice } from '../../store/slices'
+import { useAppDispatch, useAppSelector } from '../../store/hooks'
+import { CSSProperties } from 'react'
 
 type Props = {
   title: string
   posterImg: string
   description: string
+  id: string
 }
 
-export const Movie = ({ title, description, posterImg }: Props) => {
+const iconStyles: CSSProperties = {
+  fontSize: 24,
+  color: 'gold',
+}
+
+export const Movie = ({ title, description, posterImg, id }: Props) => {
+  const dispatch = useAppDispatch()
+  const allFavorites = useAppSelector((state) => state.favorites.items)
+  const isFavorite = allFavorites.find((favMovie) => favMovie.id === id)
+
+  const handleAddToFavorites = (
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>
+  ) => {
+    e.preventDefault()
+    dispatch(
+      favoritesSlice.actions.addToFavorites({
+        title,
+        description,
+        posterImg,
+        id,
+      })
+    )
+  }
+
+  const handleRemoveFromFavorites = (
+    e: React.MouseEvent<HTMLSpanElement, MouseEvent>
+  ) => {
+    e.preventDefault()
+    dispatch(
+      favoritesSlice.actions.removeFromFavorites({
+        id,
+      })
+    )
+  }
+
   return (
     <Card
       hoverable
+      actions={[
+        isFavorite ? (
+          <HeartFilled style={iconStyles} onClick={handleRemoveFromFavorites} />
+        ) : (
+          <HeartOutlined style={iconStyles} onClick={handleAddToFavorites} />
+        ),
+      ]}
       style={{
         display: 'flex',
+        position: 'relative',
         flexDirection: 'column',
         justifyContent: 'space-between',
         width: '100%',
